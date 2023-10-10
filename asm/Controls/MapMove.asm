04fb4f71042ecda3d57afa743c378335881b438b
G. Reinoso <gabriel.reinoso.1996@gmail.com>
2023-10-10 01:03:10 -0300

    fix camera and map inputs

--- asm/Controls/MapMove.asm
+++ asm/Controls/MapMove.asm
@@ -1,3 +1,8 @@
-.org 0x0011d3f8
+.org 0x0011d3e0
+    addiu   sp, sp, -0x10
+    sd      ra, 0x0(sp)
     jal MapMoveCheck
     nop
+    ld      ra, 0x0(sp)
+    jr      ra
+    addiu   sp, sp, 0x10
--- asm/Extra/ButtonFuncs.asm
+++ asm/Extra/ButtonFuncs.asm
@@ -16,16 +16,29 @@
     nop
 
 .definelabel MapMoveCheck,.
+    lw      a1, -0x3934(gp)
+    lbu     v1, 0x8A(a1)
+    bnez    v1, @@End
+    nop
+    lbu     v1, 0x88(a1)
+    bnez    v1, @@End
     li      v1, OptScheme
     lbu     v1, 0x0(v1)
     beqz    v1, @@Analog
     nop
     andi    v1, a0, 0xFFFF
-    b       @@End
+    b       @@Do
     andi    v1, v1, BtnL3
 @@Analog:
     andi    v1, a0, 0xFFFF
     andi    v1, v1, BtnSelect
+@@Do:
+    beqz    v1, @@End
+    nop
+    lbu     v1, 0x3E(a1)
+    xori    v1, v1, 0x1
+    sb      v1, 0x3E(a1)
+    nop
 @@End:
     jr      ra
     nop
@@ -38,7 +51,7 @@
 @@DoFace:
     andi    v1, s1, 0xFFFF
     b       @@End
-    andi    v1, v1, BtnL3
+    andi    v1, v1, BtnR3
 @@DoAnalog:
     andi    v1, s1, 0xFFFF
     andi    v1, v1, 0x100
@@ -54,7 +67,7 @@
 @@DoFace:
     andi    v1, s0, 0xFFFF
     b       @@End
-    andi    v1, v1, BtnL3
+    andi    v1, v1, BtnR3
 @@DoAnalog:
     andi    v1, s0, 0xFFFF
     andi    v1, v1, 0x100
